fe9c6d29126c9219949ad42d33722109a88b9886
Wachira <dannielmutua00@gmail.com>
2023-06-09 08:39:03 +0300

    added components

--- juristech/src/App.js
+++ juristech/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Landing from './Components/Landing';
 
 function App() {
   return (
     <div className="App">
       <h1>JurisTech</h1>
+      <Landing />
+    
     </div>
   );
 }
---
+++ juristech/src/Components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landing.css';
+
+const Landing = () => {
+  return (
+    <div className="landing">
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </h1>
+        </div>
+  )
+}
+
+export default Landing
