ee19e3dd73b180b50998c6a75939db2fc7a9b933
rose karemeri <rozzy@roses-MacBook-Air.local>
2023-06-14 21:49:26 +0300

    change image

--- juristech/src/App.js
+++ juristech/src/App.js
@@ -1,26 +1,21 @@
-import './App.css';
-import Landing from './Components/Landing/Landing';
-import About from './Components/About/About'
-import Lawyers from './Components/LYT/Lawyers';
+import "./App.css";
+import Landing from "./Components/Landing/Landing";
+import About from "./Components/About/About";
+import Lawyers from "./Components/LYT/Lawyers";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-
-
 
 function App() {
   return (
     <BrowserRouter>
-          <div className="App">
-        
-  
+      <div className="App">
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/about" element={<About />} />
-           <Route exact path="/lawyer" element={<Lawyers />} />  
+          <Route exact path="/lawyer" element={<Lawyers />} />
         </Routes>
-        
       </div>
+    </BrowserRouter>
   );
-
 }
 
 export default App;
--- juristech/src/Components/LYT/Lawyers.js
+++ juristech/src/Components/LYT/Lawyers.js
@@ -1,21 +1,21 @@
 import React from "react";
-import image from "../../assets/Image.png";
+import image from "../../assets/newimg.jpg";
 
 const Lawyers = () => {
   return (
-    <div style={{ padding:"2%" }}>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <div style={{ padding:"12px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems:"center" }}>
         <div style={{ width: "40%",textAlign:"left" }}>
           <h1 style={{fontWeight:"700",fontSize:"40px"}}>Lawyers you <br /> can trust</h1>
         </div>
-        <div style={{ width: "50%", textAlign:"left" }}>
-          <p>
+        <div style={{ width: "50%", textAlign:"left", fontSize:"14px" }}>
+          <p style={{fontSize:"14px"}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros elementum tristique. Duis cursus, mi quis
             viverra ornare, eros dolor interdum nulla, ut commodo diam libero
             vitae erat.
           </p>
-          <p>
+          <p style={{fontSize:"14px"}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros elementum tristique. Duis cursus, mi quis
             viverra ornare, eros dolor interdum nulla, ut commodo diam libero
@@ -28,18 +28,17 @@ const Lawyers = () => {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          textAlign:"center",
-          height: "70vh",
-          backgroundColor:"green"
+          height: "50%",
+          // width:"80%"
         }}
       >
         <img
           src={image}
           alt="this is an "
           style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
+            // width: "100%",
+            // height: "100%",
+            // objectFit: "cover",
           }}
         />
       </div>
