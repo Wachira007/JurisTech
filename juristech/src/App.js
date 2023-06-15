7073ded30ef69265e7560ebf827fa5532860cb74
Wachira <dannielmutua00@gmail.com>
2023-06-15 20:08:54 +0300

    changes

--- juristech/src/App.js
+++ juristech/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 // import Landing from "./Components/Landing/Landing";
 import About from "./Components/About/About";
 // import Lawyers from "./Components/LYT/Lawyers";
-// import Practice from "./Components/Practice_area/Practice";
+import Practice from "./Components/Practice_area/Practice";
 // import Contact from "./Components/ContactUs/Contact";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
@@ -17,6 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<PageOne />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/practice" element={<Practice />} />
           {/* <Route exact path="/about" element={<About />} />
           <Route exact path="/lawyer" element={<Lawyers />} />
           <Route exact path="/practice" element={<Practice />} />
--- juristech/src/Components/Practice_area/Practice.js
+++ juristech/src/Components/Practice_area/Practice.js
@@ -20,8 +20,9 @@ const Practice = () => {
               display: "flex",
               flexDirection: "column",
               backgroundColor: "pink",
-              padding: "15px",
+              padding: "25px",
               marginRight: "30px",
+              height: "350px",
               textAlign: "left",
             }}
           >
@@ -37,14 +38,14 @@ const Practice = () => {
               flexDirection: "column",
               backgroundColor: "black",
               color: "#fff",
-              padding: "15px",
+              padding: "25px",
+              height: "350px",
               textAlign: "left",
             }}
           >
-            <h3>Divorce Law</h3>
+            <h3>Civil Law</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisses varius enim in eros elementum tristique.
+            Our Civil Law practice covers a broad spectrum of areas, including contract disputes, tort claims, property disputes, employment law matters, personal injury cases, and more. Whether you are seeking compensation, defending your rights, or seeking resolution through negotiation or litigation, our experienced civil law attorneys will work diligently to protect your interests and achieve the best possible outcome for your case
             </p>
           </div>
         </div>
@@ -54,15 +55,16 @@ const Practice = () => {
               display: "flex",
               flexDirection: "column",
               backgroundColor: "grey",
-              padding: "15px",
+              padding: "25px",
+              height: "350px",
               marginRight: "30px",
               textAlign: "left",
             }}
           >
-            <h3>Injury Law</h3>
+            
+            <h3>Corporate Law</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisses varius enim in eros elementum tristique.
+            Our Corporate Law practice covers a wide range of areas, including entity formation, contract drafting and negotiation, mergers and acquisitions, corporate restructuring, shareholder agreements, corporate governance, and regulatory compliance. We assist clients in structuring their businesses, ensuring compliance with applicable laws and regulations, and mitigating risks associated with corporate transactions. With Juristech's practical and strategic legal advice that aligns with your business objectives, you can navigate the corporate landscape with confidence.
             </p>
           </div>
           <div
@@ -70,14 +72,24 @@ const Practice = () => {
               display: "flex",
               flexDirection: "column",
               backgroundColor: "#fffdd0",
-              padding: "15px",
+              padding: "25px",
+              height: "350px",
               textAlign: "left",
             }}
           >
-            <h3>Civil Law</h3>
+            <h3>Divorce Law</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisses varius enim in eros elementum tristique.
+            Our Civil Law practice covers a broad spectrum of 
+            areas, including contract disputes, tort claims, 
+            property disputes, employment law matters, personal
+            injury cases, and divorce law. With a specialization
+            in Divorce Law, Juristech offers expertise in helping
+            clients navigate the complexities of divorce proceedings,
+            division of assets, child custody, alimony, and related
+            matters. Whether you are seeking compensation, defending 
+            your rights, or seeking resolution through negotiation or 
+            litigation, our experienced civil law attorneys will work 
+            diligently to protect your interests and achieve the best possible outcome for your case.
             </p>
           </div>
         </div>
