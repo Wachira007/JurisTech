bc002126f58da5ade385d3931f2b1bc523f4bae8
Wachira <dannielmutua00@gmail.com>
2023-06-15 13:42:51 +0300

    updated about

--- juristech/src/Components/About/About.js
+++ juristech/src/Components/About/About.js
@@ -1,21 +1,68 @@
 import React from 'react'
 import './about.css';
 
-const Landing = () => {
+
+
+const About = () => {
   return (
-    <div className="about">
+   <div className="about">
             
-        <h1>What we Believe in </h1>
+        <h1>What we Believe in </h1> 
+        <br></br>
         <h2>JurisTech - Bridging Technology and Law for a Connected Future</h2>
+        <br></br>
        <div className="statement"> 
-       <p>JurisTech is a leading consultancy firm specializing in the intersection of technology and the law. With a team of highly skilled professionals, we offer comprehensive services to businesses, organizations, and individuals seeking expert guidance in navigating the legal complexities of the rapidly evolving technological landscape.</p>
+       
        <img src="https://images.pexels.com/photos/159832/justice-law-case-hearing-159832.jpeg?auto=compress&cs=tinysrgb&w=600" alt="alternatetext"></img>
        </div>
+       <div id='us1'>
+       <p>JurisTech is a leading consultancy firm specializing in the intersection of technology and the law. With a team of highly skilled professionals, we offer comprehensive services to businesses, organizations, and individuals seeking expert guidance in navigating the legal complexities of the rapidly evolving technological landscape.</p>
+       </div>
        
+       <div id = "firm">
+        <h2>Our Firm <br/> Great advocates at your service
+        </h2>
+        
+       </div>
+       <div id ="weare"> 
        
+       <img src="https://images.pexels.com/photos/3823444/pexels-photo-3823444.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Managing Partner"></img>
+       <img src="https://images.pexels.com/photos/3823444/pexels-photo-3823444.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Senior Partner"></img>
+       <img src="https://images.pexels.com/photos/3823444/pexels-photo-3823444.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Senior Partner"></img>
        
+       </div>
+       <div id = "partnernames">
+       <h4>Daniel Wachira <br/> Senior Partner</h4>
+       <h4>Daniel Wachira <br/> Managing Partner </h4>
+       <h4>Daniel Wachira <br/> Senior Partner </h4>
+       
+       </div>
+
+       <div id = "roles">
+       <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat.</h4>
+       <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat.</h4>
+       <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat.</h4>
+       </div>
+          
+       <div id = "firm">
+        <h2>Our Practice Areas</h2>
         </div>
+       </div>
+      
+        
+       
+       
+        
   )
 }
 
-export default Landing
+export default About
