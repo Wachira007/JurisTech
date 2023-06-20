bf81a96879af314d8c503795955d66ba8e703c3a
Wachira <dannielmutua00@gmail.com>
2023-06-20 20:27:17 +0300

    practce law

---
+++ juristech/src/Pages/PageFive.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Practice from '../Components/Practice_area/Practice'
+
+const PageFive = () =>{
+    return (
+
+        <div>
+            <Practice/>
+        </div>
+      )
+}
+
+export default PageFive
